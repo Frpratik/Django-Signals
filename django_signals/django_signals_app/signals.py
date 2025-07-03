894f520c2dbaa06c50f0ae730e2d5d737a334156
Pratik <ghugepratik52@gmail.com>
2025-07-03 23:10:40 +0530

    Question 2: Prove Django signals run in same thread as caller

--- django_signals/django_signals_app/signals.py
+++ django_signals/django_signals_app/signals.py
@@ -3,9 +3,11 @@ from django.dispatch import receiver
 from .models import DemoModel
 
 import time
+import threading  # We have imported threding to get ID number of current thred.
 
 @receiver(post_save, sender=DemoModel)
 def my_signal(sender, instance, **kwargs):
     print("=== Signal started ===")
-    time.sleep(3)
+    print(f"Signal thread ID: {threading.get_ident()}") 
+    time.sleep(2)
     print("=== Signal finished ===")
--- django_signals/django_signals_app/views.py
+++ django_signals/django_signals_app/views.py
@@ -1,14 +1,17 @@
 from django.http import JsonResponse
 from .models import DemoModel
+import threading
 
 def signal_api(request):
     print("=== Before saving in API ===")
+    print(f"View thread ID: {threading.get_ident()}")
     DemoModel.objects.create(name="Triggered by API")
     print("=== After saving in API ===")
     return JsonResponse({"message": "Check server logs!"})
 
 
 """ 
+Q.1
 This simple API proves that Django signals are synchronous.
 
  How it works:
@@ -22,4 +25,28 @@ This simple API proves that Django signals are synchronous.
    you'll see that the signal runs BEFORE the "After saving" line prints.
     
  -> This proves that Django signals are synchronous by default.
+"""
+
+
+"""
+Q.2
+This API demonstrates whether Django signals run in the same thread as the caller.
+
+How it works:
+1. When you call this API endpoint (/api/test-signal/), it prints the thread ID of the view (caller).
+2. The view saves a model instance, which triggers a signal.
+3. The signal handler prints its own thread ID.
+4. If both thread IDs are the same, it confirms that Django signals run in the same thread by default.
+
+Check your server logs for the output.
+
+o/p -> 
+=== Before saving in API ===
+View thread ID: 9504
+=== Signal started ===
+Signal thread ID: 9504
+=== Signal finished ===
+=== After saving in API ===
+
+So, see in above op we are getting a same id(9504).
 """
